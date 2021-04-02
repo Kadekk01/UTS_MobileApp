7e2bd3f7ab3258142f88f9b454dd38eaaa35f7e9
Kadekk01 <dekyudiarsana26@gmail.com>
2021-04-02 23:13:23 +0800

    Menambahkan if statement agar tidak terjadi error

--- lib/main.dart
+++ lib/main.dart
@@ -28,36 +28,63 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-     {
-       'questionText': 'Ada berapa warna dalam pelangi?',
-       'answers': ['7 Warna', '6 Warna', '2 Warna', '8 Warna', '10 Warna'],
-     },
-     {
-       'questionText': 'Mau pilih siapa, aku atau dia?',
-       'answers': ['Kamu donk', 'Dia aja'],
-     },
-     {
-       'questionText': 'Tujuan anda hidup apa?',
-       'answers': ['Rebahan', 'Wanita', 'Pahala', 'Uang'],
-     },
-   ];
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Aplikasi quiz'),
-       ),
-       body: Column(
-         children: [
-           Question(
-             questions[_questionIndex]['questionText'],
-           ),
-           ...(questions[_questionIndex]['answers'] as List<String>)
-               .map((answer) {
-             return Answer(_answerQuestion, answer);
-           }).toList()
-         ],
-       ),
-     ),
-   );
- }
+      {
+        'questionText': 'Ada berapa warna dalam pelangi?',
+        'answers': ['7 Warna', '6 Warna', '2 Warna', '8 Warna', '10 Warna'],
+      },
+      {
+        'questionText': 'Mau pilih siapa, aku atau dia?',
+        'answers': ['Kamu donk', 'Dia aja'],
+      },
+      {
+        'questionText': 'Tujuan anda hidup apa?',
+        'answers': ['Rebahan', 'Wanita', 'Pahala', 'Uang'],
+      },
+    ];
+
+    var _questionIndex = 0;
+
+    void _answerQuestion() {
+      // var aBool = true;
+      // aBool = false;
+
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+      print(_questionIndex);
+      if (_questionIndex < questions.length) {
+        print('Kita masih punya pertanyaan!');
+      } else {
+        print('tidak ada pertanyaan lagi!');
+      }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      // var dummy = const ['Hello'];
+      // dummy.add('Max');
+      // print(dummy);
+      // dummy = [];
+      // questions = []; // does not work if questions is a const
+
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Aplikasi quiz'),
+          ),
+          body: Column(
+            children: [
+              Question(
+                questions[_questionIndex]['questionText'],
+              ),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+            ],
+          ),
+        ),
+      );
+    }
+  }
 }
