492ce96919fa4a2e5bbf3d2a11935f8c61b4fb9c
Kadekk01 <dekyudiarsana26@gmail.com>
2021-04-02 21:54:34 +0800

    import custom widget ke main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
+
+import './question.dart';
+// void main() {
+//   runApp(MyApp());
+// }
  
-void main() {
- runApp(MyApp());
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatefulWidget {
+ @override
+ State<StatefulWidget> createState() {
+   return _MyAppState();
+ }
 }
  
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+ var _questionIndex = 0;
+ 
+ void _answerQuestion() {
+   setState(() {
+     _questionIndex = _questionIndex + 1;
+   });
+   print(_questionIndex);
+ }
+ 
+ @override
  Widget build(BuildContext context) {
-   return MaterialApp(home: Text('hello world'));
+   var questions = [
+     'Apa warna favorit anda?',
+     'apa peliharaan favorit anda?',
+   ];
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text('Aplikasi quiz'),
+       ),
+       body: Column(
+         children: [
+            Question(
+             questions[_questionIndex],
+           ),
+           ElevatedButton(
+             child: Text('jawab 1'),
+             onPressed: _answerQuestion,
+           ),
+           ElevatedButton(
+             child: Text('jawab 2'),
+             onPressed: () => print('jawab 2 dipilih!'),
+           ),
+           ElevatedButton(
+             child: Text('jawab 3'),
+             onPressed: () {
+               print('jawab 3 dipilih');
+             },
+           ),
+         ],
+       ),
+     ),
+   );
  }
 }
