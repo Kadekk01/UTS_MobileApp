5ec82c06760731e673c9d635275f96cf73b299d7
Kadekk01 <dekyudiarsana26@gmail.com>
2021-04-02 22:48:41 +0800

    Memasukkan custom widget lagi pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,63 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 // void main() {
 //   runApp(MyApp());
 // }
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatefulWidget {
- @override
- State<StatefulWidget> createState() {
-   return _MyAppState();
- }
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
- 
+
 class _MyAppState extends State<MyApp> {
- var _questionIndex = 0;
- 
- void _answerQuestion() {
-   setState(() {
-     _questionIndex = _questionIndex + 1;
-   });
-   print(_questionIndex);
- }
- 
- @override
- Widget build(BuildContext context) {
-   var questions = [
-     'Apa warna favorit anda?',
-     'apa peliharaan favorit anda?',
-   ];
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Aplikasi quiz'),
-       ),
-       body: Column(
-         children: [
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'Apa warna favorit anda?',
+      'apa peliharaan favorit anda?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Aplikasi quiz'),
+        ),
+        body: Column(
+          children: [
             Question(
-             questions[_questionIndex],
-           ),
-           ElevatedButton(
-             child: Text('jawab 1'),
-             onPressed: _answerQuestion,
-           ),
-           ElevatedButton(
-             child: Text('jawab 2'),
-             onPressed: () => print('jawab 2 dipilih!'),
-           ),
-           ElevatedButton(
-             child: Text('jawab 3'),
-             onPressed: () {
-               print('jawab 3 dipilih');
-             },
-           ),
-         ],
-       ),
-     ),
-   );
- }
+              questions[_questionIndex],
+            ),
+            ElevatedButton(
+              child: Text('jawab 1'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('jawab 2'),
+              onPressed: () => print('jawab 2 dipilih!'),
+            ),
+            ElevatedButton(
+              child: Text('jawab 3'),
+              onPressed: () {
+                print('jawab 3 dipilih');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
